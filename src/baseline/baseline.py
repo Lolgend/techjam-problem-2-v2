3fd6ba13de2bc04decbc19fd5a73fe63f5ecc396
Zhi Wei <lumzhiwei@gmail.com>
2026-09-01 02:35:26 +0800

    .

--- src/baseline/baseline.py
+++ src/baseline/baseline.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument(
         "--data_dir",
-        default="./KuaiRand-Pure-dataset/data",
+        default="./input",
         help="KuaiRand-Pure 解压后的 data 目录",
     )
     ap.add_argument("--model", default="fm", choices=["pop", "fm", "random"])
